2595a3fd6420104a7ed7cdeab430c86f58405634
tehreemtalat7 <tehreem.talat@7vals.com>
2019-07-05 16:03:23 +0500

    Secon time commit, book chapter 2 changes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
